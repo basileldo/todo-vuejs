3a2e64cd5e23a4d5ca6580d68079804305bdd5da
BASIL ELDO <basileldo@example.com>
2024-06-03 14:03:25 +0100

    Added PWA settings in the vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  publicPath: "./",
+  pwa: {
+    name: 'Vue ToDo App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      background_color: '#42B883'
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
+    }
+  }
 };
