2fe7bbfc44f6dd05e4dd260482c7f6c5dd602caa
BASIL ELDO <basileldo@example.com>
2024-06-04 12:33:54 +0100

    Changed for component to fetch data from the backend and post new todos

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent="onSubmit">
+  <div>
     <h2 class="label-wrapper">
       <label for="new-todo-input" class="label__lg">
         What needs to be done?
@@ -11,26 +11,57 @@
       name="new-todo"
       autocomplete="off"
       v-model.lazy.trim="label"
+      v-model="newTodo"
       class="input__lg" />
     <button type="submit" class="btn btn__primary btn__lg">Add</button>
-  </form>
+    <ul>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+        @remove="removeTodo(todo._id)"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
+import TodoItem from './ToDoItem.vue';
+import axios from 'axios';
+
 export default {
-  methods: {
-    onSubmit() {
-      if (this.label === "") {
-        return;
-      }
-      this.$emit("todo-added", this.label);
-      this.label = "";
-    },
-  },
+  components: { TodoItem },
   data() {
     return {
-      label: "",
+      newTodo: '',
+      todos: []
     };
   },
+  async created() {
+    this.fetchTodos();
+  },
+  methods: {
+    async fetchTodos() {
+      const response = await axios.get('http://localhost:5000/todos');
+      this.todos = response.data;
+    },
+    async addTodo() {
+      if (this.newTodo.trim()) {
+        const response = await axios.post('http://localhost:5000/todos', {
+          task: this.newTodo
+        });
+        this.todos.push(response.data);
+        this.newTodo = '';
+      }
+    },
+    async removeTodo(id) {
+      await axios.delete(`http://localhost:5000/todos/${id}`);
+      this.todos = this.todos.filter(todo => todo._id !== id);
+    }
+  }
 };
 </script>
+
+<style scoped>
+/* Add your styles here */
+</style>
